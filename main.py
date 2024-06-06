ffff1090eae6c5034bc931422c1915e57208b7d8
2000shang <144838203+2000shang@users.noreply.github.com>
2024-06-06 02:21:58 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import click
-from lib import stock_info
+from lib import stock_info, covid_news_info 
 from utils import GraphExt, save_graph_to_file, add_dates_to_graph
 
 
@@ -9,14 +9,23 @@ def cli_app():
 
 
 @cli_app.command()
-@click.option("--start-date", default="2020-01-01", help="Start date for stock data")
-@click.option("--end-date", default="2020-01-31", help="End date for stock data")
+@click.option("--start-date", default="2020-12-12", help="Start date for stock data")
+@click.option("--end-date", default="2021-01-31", help="End date for stock data")
 def stock_data_to_ttl(start_date, end_date):
     g = GraphExt()
     add_dates_to_graph(g, start_date=start_date, end_date=end_date)
     stock_info.import_sp_stock_data(g, start_date=start_date, end_date=end_date)
     save_graph_to_file(g, "artifacts/stock_data.ttl")
 
+
+
+@cli_app.command()
+@click.option("--start-date", default="2020-01-01", help="Start date for vaccination news data")
+@click.option("--end-date", default="2020-12-31", help="End date for vaccination news data")
+def vaccination_news_to_ttl(start_date, end_date):
+    g = GraphExt()
+    covid_news_info.import_vaccination_news_data(g, start_date=start_date, end_date=end_date)
+    save_graph_to_file(g, "artifacts/vaccination_news_data.ttl")
 
 if __name__ == "__main__":
     cli_app()
